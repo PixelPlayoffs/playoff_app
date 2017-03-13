a44add1e3fc474bd334f79f18e2e2ac70958b1c6
tripdubroot <ian.philpot@microsoft.com>
2017-03-13 13:02:41 -0400

    passing tests

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,20 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+  let videoSource = '//amssamples.streaming.mediaservices.windows.net/91492735-c523-432b-ba01-faba6c2206a2/AzureMediaServicesPromo.ism/manifest';
+  let match = [
+    'Artist One',
+    'Artist Two'
+  ];
+  let tally = [
+    '98',
+    '103'
+  ];
+
+  ReactDOM.render(<App 
+                    videoSource={videoSource} 
+                    match={match} 
+                    tally={tally} />,
+  div);
 });
